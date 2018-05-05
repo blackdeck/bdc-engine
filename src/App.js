5f1554832d3f558a4cd00cb842bc5e491c0d0c6a
Maxim <iamwhaler8@gmail.com>
2018-05-05 15:57:42 +0300

    footer as a separate component (turn 1)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import _ from 'lodash';
 import { Tooltip, OverlayTrigger, Modal } from 'react-bootstrap';
+import Footer from './footer.js'
+
 
 import './css/App.css';
+import './css/footer.css';
+
+
 
 import {game_name, social_links, support} from './game/app_config';
 import {getDefaultState} from './game/default_state';
@@ -332,35 +337,7 @@ class App extends Component {
                     </div>
                 </div>
 
-                <div className="footer">
-                    <span className="pull-left"><a href="#" onClick={this.newGame} title='Hard Reset For Developers'>New game</a></span>
-
-                    &nbsp;
-                    <a target="_blank" rel="noopener noreferrer" href={social_links.telegram}>
-                        <img alt="" src="http://www.advanceduninstaller.com/7b12b396d38166a899fff585e466e50d-icon.ico" />
-                        &nbsp;
-                        Telegram
-                    </a>
-                    &nbsp;&nbsp;&nbsp;
-                    <a target="_blank" rel="noopener noreferrer" href={social_links.wiki}>
-                        <img alt="" src="https://static.filehorse.com/icons-web/educational-software/wikipedia-icon-32.png" />
-                        &nbsp;
-                        Wiki
-                    </a>
-                    &nbsp;&nbsp;&nbsp;
-                    <a target="_blank" rel="noopener noreferrer" href={social_links.reddit}>
-                        <img alt="" src="https://images-na.ssl-images-amazon.com/images/I/418PuxYS63L.png" />
-                        &nbsp;
-                        Reddit
-                    </a>
-                    &nbsp;&nbsp;&nbsp;
-                    <a rel="noopener noreferrer" href={support.show}>
-                        <img alt="" src="https://image.flaticon.com/icons/svg/81/81924.svg"/>
-                        &nbsp;
-                        Support
-                    </a>
-
-                </div>
+            <Footer />
             </div>
         );
     }
---
+++ src/footer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './css/footer.css';
+import {social_links, support} from './game/app_config';
+
+
+class Footer extends Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    render() {
+        return (
+            <div className="footer">
+                <span className="pull-left"><a href="#" onClick={this.newGame} title='Hard Reset For Developers'>New game</a></span>
+
+                &nbsp;
+                <a target="_blank" rel="noopener noreferrer" href={social_links.telegram}>
+                    <img alt="" src="http://www.advanceduninstaller.com/7b12b396d38166a899fff585e466e50d-icon.ico" />
+                    &nbsp;
+                    Telegram
+                </a>
+                &nbsp;&nbsp;&nbsp;
+                <a target="_blank" rel="noopener noreferrer" href={social_links.wiki}>
+                    <img alt="" src="https://static.filehorse.com/icons-web/educational-software/wikipedia-icon-32.png" />
+                    &nbsp;
+                    Wiki
+                </a>
+                &nbsp;&nbsp;&nbsp;
+                <a target="_blank" rel="noopener noreferrer" href={social_links.reddit}>
+                    <img alt="" src="https://images-na.ssl-images-amazon.com/images/I/418PuxYS63L.png" />
+                    &nbsp;
+                    Reddit
+                </a>
+                &nbsp;&nbsp;&nbsp;
+                <a rel="noopener noreferrer" href={support.show}>
+                    <img alt="" src="https://image.flaticon.com/icons/svg/81/81924.svg"/>
+                    &nbsp;
+                    Support
+                </a>
+
+            </div>
+        )
+    }
+
+}
+
+export default Footer
