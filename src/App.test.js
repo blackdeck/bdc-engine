674339e34a3d6f239ff7b6c18dc7ff73fbbe45c4
polesskiy-dev <e34dream@gmail.com>
2018-07-28 19:00:30 +0300

    fix initial test, add localStorage mock lib

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
 import App from './App';
 
-// TODO should be enabled further
-xit('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders without crashing', () => {
+    shallow(<App />);
 });
