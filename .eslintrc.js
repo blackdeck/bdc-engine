886d2702150b66405b8e6ec5b38a22f1dd6c012f
polesskiy-dev <e34dream@gmail.com>
2018-07-30 20:03:34 +0300

    add prettier with airbnb config and stylelint config

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+// .eslintrc.js is prioritised over other eslintrc formats
+
+module.exports = {
+    'env': {
+        'browser': true,
+        'jest': true,
+        'es6': true,
+        'node': true,
+    },
+    'extends': [
+        'airbnb',
+        'prettier',
+    ],
+    'plugins': [
+        'prettier',
+    ],
+    'rules': {
+        'prettier/prettier': ['error', {
+            'singleQuote': true,
+            'trailingComma': 'es5'
+        }],
+    },
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true,
+        }
+    }
+}
