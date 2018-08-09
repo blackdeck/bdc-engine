588bf852084dd1599fbcb253ab7eb19ae703146f
polesskiy-dev <e34dream@gmail.com>
2018-08-10 00:17:36 +0300

    add normalize.css and improve initial css folders

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import Footer from '../layout/Footer/Footer';
 
+import '../../css/main.css';
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="foo">hello</div>
         <Footer newGame={this.newGame} />
       </div>
     );
