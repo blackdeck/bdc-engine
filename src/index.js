d58de046c9f1f09c9ebe6915534cb99c8a903cb3
polesskiy-dev <e34dream@gmail.com>
2018-08-11 21:52:23 +0300

    add redux/rxjs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'rxjs';
 
+import React from 'react';
+import { createEpicMiddleware } from 'redux-observable';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import rootReducer from './reducer.root';
+import rootEpic from './epic.root';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// store initial state
+const initialState = {};
+const epicMiddleware = createEpicMiddleware();
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(epicMiddleware))
+);
+
+epicMiddleware.run(rootEpic);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
